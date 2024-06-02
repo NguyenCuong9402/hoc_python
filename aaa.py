2f2b4eb6caddac29a1df9329008a803ee8c18227
N.N.Cuong <cuong.nguyen@boot.ai>
2024-06-02 13:46:04 +0700

    Li thuyet buoi 6

---
+++ aaa.py
@@ -0,0 +1,19 @@
+print("Hoc_git")
+
+# Tạo file .git : Để lưu lịch sử/ kết nối với kho dữ liệu.
+# git init
+
+# Kết nối với kho dữ liệu git
+# git remote add origin https://github.com/NguyenCuong9402/hoc_python.git
+
+# kiểm tra có remote thành công không
+# git remote -v
+
+
+# Add vào kho lưu trữ tạm thời
+# git add .
+# Comment để đọc lại đ ã làm gì
+# git commit -v " comment"
+
+# Đẩy các commit len git
+# git push

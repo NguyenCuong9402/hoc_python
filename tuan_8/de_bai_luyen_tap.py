2b92f99e53d0d204d56fba97b85f372fa940e6ce
Nguyen Ngoc Cuong <cuong09042002@gmail.com>
2024-06-23 17:07:43 +0700

    ok

--- tuan_8/de_bai_luyen_tap.py
+++ tuan_8/de_bai_luyen_tap.py
@@ -1,15 +1,3 @@
-# 2. Quản lý điểm sinh viên (Thông tin học sinh: tên,
-# Các môn học: toán, văn, lí, hóa, sử, địa, mỹ thuật, âm nhạc, tiếng anh)
-
-# Nhập tên sinh viên và điểm số của các môn học.
-# Tính điểm trung bình của mỗi sinh viên.
-# Số học sinh giỏi/ khá / yếu.
-# Hiển thị cơ cấu % học sinh yếu, khá, giỏi.  1-4: yếu, 4-8: kha, 8-10: gioi
-# Tìm số học sinh có số điểm > số nhập và môn học yêu cầu.
-# ví dụ: môn học hóa, điểm là 6.
-# Trả ra tất cả thông tin học sinh có số điểm hóa > 6
-# In kết quả ra màn hình.
-
 
 
 # 1. Quản lý thư viện sách
